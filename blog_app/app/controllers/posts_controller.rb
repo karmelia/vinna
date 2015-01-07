f60d75a02e0acba799672fc2fe326cc737d1cde6
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2015-01-06 17:17:23 -0800

    TODO: add images to posts, start on articles, ask Anna about social media icons, figure out giant footer margin mystery.

--- blog_app/app/controllers/posts_controller.rb
+++ blog_app/app/controllers/posts_controller.rb
@@ -44,6 +44,6 @@ class PostsController < ApplicationController
 
 	private
 		def post_params
-			params.require(:post).permit(:title, :text)
+			params.require(:post).permit(:image_file_name, :title, :text)
 		end
 end
--- blog_app/app/models/post.rb
+++ blog_app/app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
 	validates :title, presence: true,
 										length: { minimum: 2 }
+
+	validates :text, presence: true,
+										length: { minimum: 5 }
 end
