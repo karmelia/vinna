98843630e67c9ef91431148a60aad0f355df78ea
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2015-02-12 17:30:47 -0800

    continue to follow the tutorial on rubyonrailshelp in the README

--- config/routes.rb
+++ config/routes.rb
@@ -1,62 +1,56 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
+
   root 'posts#index'
 
   get 'about', to: 'about#index'
 
   resources :posts
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  match '/contacts', to: 'contacts#new', via: 'get'
+  resources "contacts", only: [:new, :create]
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #              Prefix Verb         URI Pattern                     Controller#Action
+  #         new_admin_session GET    /admins/sign_in(.:format)       devise/sessions#new
+  #             admin_session POST   /admins/sign_in(.:format)       devise/sessions#create
+  #     destroy_admin_session DELETE /admins/sign_out(.:format)      devise/sessions#destroy
+  #            admin_password POST   /admins/password(.:format)      devise/passwords#create
+  #        new_admin_password GET    /admins/password/new(.:format)  devise/passwords#new
+  #       edit_admin_password GET    /admins/password/edit(.:format) devise/passwords#edit
+  #                           PATCH  /admins/password(.:format)      devise/passwords#update
+  #                           PUT    /admins/password(.:format)      devise/passwords#update
+  # cancel_admin_registration GET    /admins/cancel(.:format)        devise/registrations#cancel
+  #        admin_registration POST   /admins(.:format)               devise/registrations#create
+  #    new_admin_registration GET    /admins/sign_up(.:format)       devise/registrations#new
+  #   edit_admin_registration GET    /admins/edit(.:format)          devise/registrations#edit
+  #                           PATCH  /admins(.:format)               devise/registrations#update
+  #                           PUT    /admins(.:format)               devise/registrations#update
+  #                           DELETE /admins(.:format)               devise/registrations#destroy
+  #          new_user_session GET    /users/sign_in(.:format)        devise/sessions#new
+  #              user_session POST   /users/sign_in(.:format)        devise/sessions#create
+  #      destroy_user_session DELETE /users/sign_out(.:format)       devise/sessions#destroy
+  #             user_password POST   /users/password(.:format)       devise/passwords#create
+  #         new_user_password GET    /users/password/new(.:format)   devise/passwords#new
+  #        edit_user_password GET    /users/password/edit(.:format)  devise/passwords#edit
+  #                           PATCH  /users/password(.:format)       devise/passwords#update
+  #                           PUT    /users/password(.:format)       devise/passwords#update
+  #  cancel_user_registration GET    /users/cancel(.:format)         devise/registrations#cancel
+  #         user_registration POST   /users(.:format)                devise/registrations#create
+  #     new_user_registration GET    /users/sign_up(.:format)        devise/registrations#new
+  #    edit_user_registration GET    /users/edit(.:format)           devise/registrations#edit
+  #                           PATCH  /users(.:format)                devise/registrations#update
+  #                           PUT    /users(.:format)                devise/registrations#update
+  #                           DELETE /users(.:format)                devise/registrations#destroy
+  #                      root GET    /                               posts#index
+  #                     about GET    /about(.:format)                about#index
+  #                     posts GET    /posts(.:format)                posts#index
+  #                           POST   /posts(.:format)                posts#create
+  #                  new_post GET    /posts/new(.:format)            posts#new
+  #                 edit_post GET    /posts/:id/edit(.:format)       posts#edit
+  #                      post GET    /posts/:id(.:format)            posts#show
+  #                           PATCH  /posts/:id(.:format)            posts#update
+  #                           PUT    /posts/:id(.:format)            posts#update
+  #                           DELETE /posts/:id(.:format)            posts#destroy
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
