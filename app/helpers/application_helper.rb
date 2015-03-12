9b12cecea985867fc7e73f98df2a333a103867f7
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2015-03-12 13:47:20 -0700

    logged_in? wasn't working so I put the logic in the view. I KNOW that's bad but it worked, okay

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,19 @@ module ApplicationHelper
 	def log_in(user)
 	  session[:user_id] = user.id
 	end
+	
+	def current_user
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
+
+	def logged_in?
+	  !current_user.nil?
+	end
+
+	def logged_in_user
+	  unless logged_in?
+	    flash[:danger] = "Please log in."
+	    redirect_to log_in_url
+	  end
+	end
 end
