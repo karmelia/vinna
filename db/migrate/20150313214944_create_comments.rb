1cde64780953020f1146d1084e218b0735f727bc
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2015-03-13 14:51:29 -0700

    added comment migration back

---
+++ db/migrate/20150313214944_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+  	drop_table :comments
+    create_table :comments do |t|
+      t.string :author_name
+      t.text :body
+      t.datetime :created_at,         null: false
+      t.datetime :updated_at,         null: false
+      t.references :post, index: true
+    end
+    add_foreign_key :comments, :posts
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312222333) do
+ActiveRecord::Schema.define(version: 20150313214944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string   "author_name"
-    t.text     "body"
-    t.integer  "post_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string  "author_name"
+    t.text    "body"
+    t.integer "post_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
