932ec9bcc5c364be9a5e81c304e4d6150901c221
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2015-05-11 14:21:43 -0700

    precompiled assets

---
+++ public/assets/ckeditor/config-7427bbeaaed2619735b55a8ec95d9595.js
@@ -0,0 +1,7 @@
+CKEDITOR.editorConfig = function( config )
+{
+    config.toolbar_img = [
+        { name: 'document', items: [ 'Source', '-', 'NewPage', 'Preview', '-', 'Templates' ] },
+    ]
+}
+;
